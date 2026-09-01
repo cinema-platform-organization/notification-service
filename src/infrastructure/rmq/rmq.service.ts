a1fd0af87d8d58868a8d3d42eceac097ff639865
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-09-01 23:32:32 +0300

    Add metrics collection

--- src/infrastructure/rmq/rmq.service.ts
+++ src/infrastructure/rmq/rmq.service.ts
@@ -1,12 +1,25 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { RmqContext } from "@nestjs/microservices";
+import { InjectMetric } from "@willsoto/nestjs-prometheus";
 import type { Channel, Message } from "amqplib";
+import { Counter } from "prom-client";
 
 @Injectable()
 export class RmqService {
+	private readonly SERVICE_NAME: string;
+
 	private readonly logger = new Logger(RmqService.name);
 
-	public ack(context: RmqContext): void {
+	public constructor(
+		@InjectMetric("rmq_events_ack_total")
+		private readonly ackTotal: Counter<string>,
+		@InjectMetric("rmq_events_nack_total")
+		private readonly nackTotal: Counter<string>,
+	) {
+		this.SERVICE_NAME = "notification";
+	}
+
+	public ack(context: RmqContext, event: string): void {
 		const channel = context.getChannelRef() as Channel;
 		const msg = context.getMessage() as Message;
 		const tag = msg?.fields?.deliveryTag;
@@ -17,12 +30,17 @@ export class RmqService {
 
 		channel.ack(msg);
 
+		this.ackTotal.inc({
+			service: this.SERVICE_NAME,
+			event,
+		});
+
 		this.logger.debug(
 			`ACK (pattern: ${context.getPattern()}, tag: ${tag})`,
 		);
 	}
 
-	public nack(context: RmqContext, requeue = false): void {
+	public nack(context: RmqContext, event: string, requeue = false): void {
 		const channel = context.getChannelRef() as Channel;
 		const msg = context.getMessage() as Message;
 		const tag = msg?.fields?.deliveryTag;
@@ -32,6 +50,11 @@ export class RmqService {
 		}
 
 		channel.nack(msg, false, requeue);
+
+		this.ackTotal.inc({
+			service: this.SERVICE_NAME,
+			event,
+		});
 
 		if (requeue) {
 			this.logger.warn(
--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -5,6 +5,8 @@ import type {
 } from "@cinema-platform/contracts";
 import { Controller, Logger } from "@nestjs/common";
 import { Ctx, EventPattern, Payload, RmqContext } from "@nestjs/microservices";
+import { InjectMetric } from "@willsoto/nestjs-prometheus";
+import { Counter, Histogram } from "prom-client";
 
 import { RmqService } from "@/infrastructure/rmq/rmq.service";
 
@@ -12,30 +14,58 @@ import { NotificationsService } from "./notifications.service";
 
 @Controller()
 export class NotificationsController {
+	private readonly SERVICE_NAME: string;
+
 	private readonly logger = new Logger(NotificationsController.name);
 
 	public constructor(
 		private readonly notificationsService: NotificationsService,
 		private readonly rmqService: RmqService,
-	) {}
+		@InjectMetric("rmq_event_processing_duration_seconds")
+		private readonly processingDuration: Histogram<string>,
+		@InjectMetric("rmq_events_total")
+		private readonly eventsTotal: Counter<string>,
+	) {
+		this.SERVICE_NAME = "notification";
+	}
 
 	@EventPattern("auth.otp.requested")
 	public async otpRequested(
 		@Payload() data: OtpRequestedEvent,
-		@Ctx() ctx: unknown,
+		@Ctx() ctx: RmqContext,
 	) {
-		const context = ctx as RmqContext;
+		const event = "auth.otp.requested";
+		const endTimer = this.processingDuration.startTimer({
+			service: this.SERVICE_NAME,
+			event,
+		});
 
 		try {
 			await this.notificationsService.sendOtp(data);
 
-			this.rmqService.ack(context);
+			this.eventsTotal.inc({
+				service: this.SERVICE_NAME,
+				event,
+				status: "success",
+			});
+
+			this.rmqService.ack(ctx, event);
 		} catch (error) {
+			this.eventsTotal.inc({
+				service: this.SERVICE_NAME,
+				event,
+				status: "error",
+			});
+
 			const message =
 				error instanceof Error ? error.message : String(error);
 
 			this.logger.error("OTP processing error: ", message);
-			this.rmqService.nack(context);
+			this.rmqService.nack(ctx, event);
+
+			throw error;
+		} finally {
+			endTimer();
 		}
 	}
 
@@ -44,16 +74,36 @@ export class NotificationsController {
 		@Payload() data: EmailChangedEvent,
 		@Ctx() ctx: RmqContext,
 	) {
+		const event = "account.email.changed";
+		const endTimer = this.processingDuration.startTimer({
+			service: this.SERVICE_NAME,
+			event,
+		});
+
 		try {
 			await this.notificationsService.sendEmailChange(data);
 
-			this.rmqService.ack(ctx);
+			this.eventsTotal.inc({
+				service: this.SERVICE_NAME,
+				event,
+				status: "success",
+			});
+
+			this.rmqService.ack(ctx, event);
 		} catch (error) {
+			this.eventsTotal.inc({
+				service: this.SERVICE_NAME,
+				event,
+				status: "error",
+			});
+
 			const message =
 				error instanceof Error ? error.message : String(error);
 
 			this.logger.error("Email change error: ", message);
-			this.rmqService.nack(ctx);
+			this.rmqService.nack(ctx, event);
+		} finally {
+			endTimer();
 		}
 	}
 
@@ -62,16 +112,36 @@ export class NotificationsController {
 		@Payload() data: PhoneChangedEvent,
 		@Ctx() ctx: RmqContext,
 	) {
+		const event = "account.email.changed";
+		const endTimer = this.processingDuration.startTimer({
+			service: this.SERVICE_NAME,
+			event,
+		});
+
 		try {
 			await this.notificationsService.sendPhoneChange(data);
 
-			this.rmqService.ack(ctx);
+			this.eventsTotal.inc({
+				service: this.SERVICE_NAME,
+				event,
+				status: "success",
+			});
+
+			this.rmqService.ack(ctx, event);
 		} catch (error) {
+			this.eventsTotal.inc({
+				service: this.SERVICE_NAME,
+				event,
+				status: "error",
+			});
+
 			const message =
 				error instanceof Error ? error.message : String(error);
 
 			this.logger.error("Phone change error: ", message);
-			this.rmqService.nack(ctx);
+			this.rmqService.nack(ctx, event);
+		} finally {
+			endTimer();
 		}
 	}
 }
